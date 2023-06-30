95e06f16e04970ca0a755be9765f807052fb4c7c
shahbaz48 <shahbaz@capabl.com>
2023-06-30 23:31:21 +0100

    updated readme.md

--- store/slices/Index.ts
+++ store/slices/Index.ts
@@ -13,11 +13,11 @@ export const counterSlice = createSlice({
   name: "stunt",
   initialState,
   reducers: {
+    // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    // doesn't actually mutate the state because it uses the Immer library,
+    // which detects changes to a "draft state" and produces a brand new
+    // immutable state based off those changes
     acceptDouble: (state, action: PayloadAction<{ data: StuntMan; currentIndex: number }>) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.acceptedDoubles = [...state.acceptedDoubles, action.payload.data];
       state.data = state.data.filter(
         (_: any, index: number) => index !== action.payload.currentIndex
